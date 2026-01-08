b6aca46d30b1f2f2a4e7031b7415f610bc82871c
Simon <37809274+Simon040101@users.noreply.github.com>
2026-01-08 07:47:05 +0100

    feat: add initial server info message (#44)

--- src/main/java/com/uni/gamesever/controller/SocketConnectionHandler.java
+++ src/main/java/com/uni/gamesever/controller/SocketConnectionHandler.java
@@ -1,17 +1,29 @@
 package com.uni.gamesever.controller;
 
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uni.gamesever.classes.MessageHandler;
+import com.uni.gamesever.models.ServerInfoEvent;
 import com.uni.gamesever.services.SocketMessageService;
 
 // Socket-Connection Configuration class
 public class SocketConnectionHandler extends TextWebSocketHandler {
     private final SocketMessageService socketBroadcastService;
     private final MessageHandler messageHandler;
+    @Value("${server.version}")
+    private String serverVersion;
+    @Value("${protocol.version}")
+    private String protocolVersion;
+    @Value("${server.motd}")
+    private String serverMotd;
+    private final ObjectMapper objectmapper = new ObjectMapper();
 
     public SocketConnectionHandler(SocketMessageService socketBroadcastService, MessageHandler messageHandler) {
         this.socketBroadcastService = socketBroadcastService;
@@ -21,15 +33,17 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // This method is executed when client tries to connect
     // to the sockets
     @Override
-    public void
-    afterConnectionEstablished(WebSocketSession session)
-        throws Exception
-    {
+    public void afterConnectionEstablished(WebSocketSession session)
+            throws Exception {
 
         super.afterConnectionEstablished(session);
         try {
             socketBroadcastService.addIncomingSession(session);
             System.out.println(session.getId() + " Connected");
+            ServerInfoEvent serverInfoEvent = new ServerInfoEvent(Instant.now().toString(), serverVersion,
+                    protocolVersion, serverMotd);
+            socketBroadcastService.sendMessageToSession(session.getId(),
+                    objectmapper.writeValueAsString(serverInfoEvent));
         } catch (Exception e) {
             throw e;
         }
@@ -39,8 +53,7 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // method is called
     @Override
     public void afterConnectionClosed(WebSocketSession session,
-                          CloseStatus status)throws Exception
-    {
+            CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
         try {
             socketBroadcastService.removeDisconnectedSession(session);
@@ -55,18 +68,17 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     // message Also the Message object passes as parameter
     @Override
     public void handleMessage(WebSocketSession session,
-                              WebSocketMessage<?> message)
-        throws Exception
-    {
+            WebSocketMessage<?> message)
+            throws Exception {
 
         super.handleMessage(session, message);
 
         System.out.println("Message Received from user " + session.getId() + ": " + message.getPayload());
 
-        //the whole game logic goes here
-        if(!messageHandler.handleClientMessage(message.getPayload().toString(), session.getId())) {
+        // the whole game logic goes here
+        if (!messageHandler.handleClientMessage(message.getPayload().toString(), session.getId())) {
             // we return a error message to the client
-            
+
         }
     }
 }
