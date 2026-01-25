70c5d4190890acf5207b1a2a5c6fd51edf5ccd64
ra0037I <ra0037@mci4me.at>
2026-01-25 11:45:05 +0100

    Fix/error output (#79)
    * Repo aufgeräumt (#77)
    
    * Repo aufgeräumt
    
    * ENV_TEMPLATE eingefügt
    
    * Clean repo (#78)
    
    * Repo aufgeräumt
    
    * ENV_TEMPLATE eingefügt
    
    * contributing entfernt und readme angepasst
    
    * Update README.md
    
    * fix: logging to file and Timestamp
    
    ---------
    
    Co-authored-by: stocki7 <62840103+stocki7@users.noreply.github.com>

--- src/main/java/com/uni/gamesever/infrastructure/AbstractTimerManager.java
+++ src/main/java/com/uni/gamesever/infrastructure/AbstractTimerManager.java
@@ -1,5 +1,8 @@
 package com.uni.gamesever.infrastructure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -9,14 +12,14 @@ public abstract class AbstractTimerManager {
 
     private ScheduledExecutorService scheduler;
     private ScheduledFuture<?> timeoutTask;
-
+    private static final Logger log = LoggerFactory.getLogger("GAME_LOG");
     protected synchronized void startInternal(long durationInSeconds, Runnable onTimeout) {
         stopInternal();
 
         scheduler = Executors.newSingleThreadScheduledExecutor();
 
         timeoutTask = scheduler.schedule(() -> {
-            System.out.println("TIMEOUT reached!");
+            log.info("TIMEOUT reached!");
             if (onTimeout != null) {
                 onTimeout.run();
             }
